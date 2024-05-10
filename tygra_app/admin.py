396663f9ade2d2358296d0708f24ead9306886bf
Leane Soares <leanesoares212@gmail.com>
2024-05-10 15:33:15 -0300

    banco de dados

--- tygra_app/admin.py
+++ tygra_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from tygra_app.models import Bairro, Carrinho, Carrinho_Produto, Categoria, Cidade, Cliente, Endereco, Estado, Imagem, NomeLogradouro, Pais, Produto, Promocao, Referencia, SubCategoria
 
-# Register your models here.
+admin.site.register(Produto)
+admin.site.register(Imagem)
+admin.site.register(Categoria)
+admin.site.register(SubCategoria)
+admin.site.register(Promocao)
+admin.site.register(Cliente)
+admin.site.register(Carrinho_Produto)
+admin.site.register(Carrinho)
+admin.site.register(Endereco)
+admin.site.register(Referencia)
+admin.site.register(Pais)
+admin.site.register(Estado)
+admin.site.register(Cidade)
+admin.site.register(Bairro)
+admin.site.register(NomeLogradouro)
